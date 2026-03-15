980a4be23afa0e979154349c77ef51d5b8ee868e
Prakash V <prakashseervi1503@gmail.com>
2026-03-15 16:06:02 +0530

    Updates

--- assets/clock-C02HiVdl.js
+++
@@ -1,1 +0,0 @@
-import{k as c}from"./index-B_atq47-.js";const e=[["path",{d:"M8 2v4",key:"1cmpym"}],["path",{d:"M16 2v4",key:"4m81vk"}],["rect",{width:"18",height:"18",x:"3",y:"4",rx:"2",key:"1hopcy"}],["path",{d:"M3 10h18",key:"8toen8"}]],a=c("calendar",e);const o=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],y=c("clock",o);export{y as C,a};
